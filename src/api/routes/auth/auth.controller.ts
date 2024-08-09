9dc6a4854398407104d7d635cb59456f042c0498
woaitsAryan <yoprince.ab@gmail.com>
2024-08-09 10:25:26 +0530

    feat: init commit

---
+++ src/api/routes/auth/auth.controller.ts
@@ -0,0 +1,31 @@
+import catchAsync from '../../../helpers/catchAsync'
+import type { Response, Request } from 'express'
+import { loginDto, type loginDtoType, registerDto, type registerDtoType } from './auth.dto'
+import { ErrorBadRequest } from '../../../helpers/errors'
+import AuthService from './auth.service'
+
+export default class AuthController {
+  public static Register = catchAsync(
+    async (req: Request, res: Response) => {
+      const validatedBody = registerDto.safeParse(req.body)
+      if (!validatedBody.success) {
+        throw new ErrorBadRequest('Invalid input')
+      }
+
+      const { token, user } = await AuthService.Register(req.body as registerDtoType)
+      return res.json({ token, user })
+    }
+  )
+
+  public static Login = catchAsync(
+    async (req: Request, res: Response) => {
+      const validatedBody = loginDto.safeParse(req.body)
+      if (!validatedBody.success) {
+        throw new ErrorBadRequest('Invalid input')
+      }
+
+      const { token, user } = await AuthService.Login(req.body as loginDtoType)
+      return res.json({ token, user })
+    }
+  )
+}
